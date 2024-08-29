a55459d8d78065d8d95ad1dfaf11e393b68fe1d6
WorldHim <worldhim@hotmail.com>
2024-08-29 08:52:27 +0800

    feat: create module

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from core.builtins import Bot
+from core.component import module
+
+obastatus = module(
+    bind_prefix='obastatus',
+    desc='{obastatus.help.desc}',
+    alias='oba',
+    developers=['WorldHim'],
+    support_languages=['zh_cn'],
+)
