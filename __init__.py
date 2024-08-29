b02b3fa4e43803a0dca5ebbfecd2c4a3e3cd5290
WorldHim <worldhim@hotmail.com>
2024-08-29 15:36:06 +0800

    feat: new command version

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from core.builtins import Bot
 from core.component import module
+from core.utils.http import get_url
 
 obastatus = module(
     bind_prefix='obastatus',
@@ -8,3 +9,12 @@ obastatus = module(
     developers=['WorldHim'],
     support_languages=['zh_cn'],
 )
+
+async def latestVersion():
+    result = await get_url('https://bd.bangbang93.com/openbmclapi/metric/version',
+                           fmt='json')
+    return result['version']
+
+@obastatus.command('version {{obastatus.help.version}}')
+async def version(msg: Bot.MessageSession):
+    await msg.finish({{obastatus.version}} + await latestVersion())
