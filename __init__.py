c152194af4bc9bf410fc1cf4fbbd3e571dc7d5ff
WorldHim <worldhim@hotmail.com>
2024-08-29 21:13:49 +0800

    docs: locale help and command_version

--- __init__.py
+++ __init__.py
@@ -13,8 +13,9 @@ obastatus = module(
 async def latestVersion():
     result = await get_url('https://bd.bangbang93.com/openbmclapi/metric/version',
                            fmt='json')
-    return result['version']
+    return result['version'] + '@' + result['_resolved'].split('#')[1][:7]
 
 @obastatus.command('version {{obastatus.help.version}}')
 async def version(msg: Bot.MessageSession):
-    await msg.finish({{obastatus.version}} + await latestVersion())
+    await msg.finish(msg.locale.t("obastatus.message.version") + ' ' + await latestVersion())
+
