924110c88c67a1bbdecd07556470154ded93033c
WorldHim <worldhim@hotmail.com>
2024-08-31 20:18:46 +0800

    fix: remove extra lines

--- __init__.py
+++ __init__.py
@@ -34,8 +34,7 @@ async def status(msg: Bot.MessageSession):
     message = f'''{msg.locale.t('obastatus.message.status.currentNodes', currentNodes = dashboard['currentNodes'])} | {msg.locale.t('obastatus.message.status.load', load = round(dashboard['load'] * 100, 2))}
     {msg.locale.t('obastatus.message.status.bandwidth', bandwidth = dashboard['bandwidth'])} | {msg.locale.t('obastatus.message.status.currentBandwidth', currentBandwidth = round(dashboard['currentBandwidth'], 2))}
     {msg.locale.t('obastatus.message.status.hits', hits = dashboard['hits'])} | {msg.locale.t('obastatus.message.status.size', size = sizeConvert(dashboard['bytes']))}
-    {msg.locale.t('obastatus.message.queryTime', queryTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S'))}
-    '''
+    {msg.locale.t('obastatus.message.queryTime', queryTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S'))}'''
 
     await msg.finish(message)
 
@@ -49,8 +48,7 @@ async def rank(msg: Bot.MessageSession, rank: int = 1):
     {msg.locale.t('obastatus.message.rank.id', id = cluster['_id'])}
     {msg.locale.t('obastatus.message.rank.hits', hits = cluster['metric']['hits'])}
     {msg.locale.t('obastatus.message.rank.size', size = sizeConvert(cluster['metric']['bytes']))}
-    {msg.locale.t('obastatus.message.queryTime', queryTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S"))}
-    '''
+    {msg.locale.t('obastatus.message.queryTime', queryTime = datetime.now().strftime("%Y-%m-%d %H:%M:%S"))}'''
 
     await msg.finish(message)
 
